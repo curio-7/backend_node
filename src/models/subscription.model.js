f1c85e22699a99e14098b843787593c58f2e8c19
Sarthak Nigam <srthkngm@gmail.com>
2024-03-04 18:09:15 +0530

    updated model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,11 +2,11 @@ import mongoose, {Schema} from "mongoose"
 
 const subscriptionSchema = new mongoose.Schema({
     subscriber: {
-        type: mongoose.Schema.Types.ObjectId, // one who is subscribing
+        type: mongoose.Schema.Types.ObjectId, // the one who is subscribing
         ref: "User"
     },
     channel: {
-        type: mongoose.Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+        type: mongoose.Schema.Types.ObjectId, // the one to whom 'subscriber' is subscribing
         ref: "User"
     }
 }, {timestamps: true})
